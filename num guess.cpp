e1db0956367f655c46659f052e2dc47ef2fe7bcb
CryptoRamz <107411663+cryptoramz@users.noreply.github.com>
2022-09-09 23:14:51 +0800

    return of num guess still a fcking newbie

---
+++ num guess.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num_guess ,fav_num;
+fav_num=69;
+cout << "Guess a fcking num between 1-70 " endl; 
+cin >> num_guess;
+if (num_guess== 69)
+{ 
+cout << "you are fcking correct" << endl;
+
+} else if (num_guess <69)
+{
+cout << "you are a fcking DUMB !"<< endl;
+}else 
+{
+cout << "you still a fcking Dumb !"<< endl;
+}
+return 0;
+} 
